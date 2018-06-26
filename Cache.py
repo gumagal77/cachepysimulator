ab3fcd025aaf6f7f4efd896e7c0ccfa651b23f5b
Diego Dantas Figueiredo <35031027+Feelss@users.noreply.github.com>
2018-06-26 20:43:35 -0300

    Update Cache.py

--- Cache.py
+++ Cache.py
@@ -1,33 +1,32 @@
+
 class Linha:
-    def __init__(self, info = None, idx = -1):
-        self.bit = False
-        self.idx = idx
-        if info is None:
-            self.tag = 0
-        else:
-            self.tag = info
+    def __init__(self, info = -1):
+        self.bit_uso = False
+        self.tag = info
 
 class Conjunto:
     def __init__(self, nro_linhas_conjunto = 1):
-        self.num_lines = nro_linhas_conjunto
-        self.linhas = [Linha() for i in range(num_lines)]
-        self.idx = 0
+        self.set = [Linha() for i in range(nro_linhas_conjunto)]
+        self.index = 0
+        self.tot_lin = nro_linhas_conjunto
+        self.fila = Queue()
 
     def isFull(self):
-        return idx == num_lines
+        return self.tot_lin == self.index
+
 
 class Cache:
     def __init__(self, linhas, palavras_linhas, nro_linhas_conjunto = None):
         self.cache_hit = 0 #Acerto de cache
         self.cache_miss = 0 #Erro de cache
         self.Memoria = 0 #Acesso a memoria
-        self.linhas = linhas
+
+        self.qtd_linhas = linhas
         self.tam_block = palavras_linhas
+
         if nro_linhas_conjunto is not None:
             self.num_conjuntos = linhas / nro_linhas_conjunto
-            self.modulos = [Conjunto(nro_linhas_conjunto) for i in range(num_conjuntos)]
-            self.associativo_conjunto = True
+            self.cache = [Conjunto(nro_linhas_conjunto) for i in range(self.num_conjuntos)]
         else:
-            self.associativo_conjunto = False
-            self.linha = [Linha() for i in range(linhas)]
+            self.cache = [Conjunto(linhas)]
 
