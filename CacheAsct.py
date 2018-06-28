7dd64e81bcc9e53f48f02a0393e22cd7b4af652f
gumagal77 <30578779+gumagal77@users.noreply.github.com>
2018-06-27 21:36:55 -0300

    Delete CacheAsct.py

--- CacheAsct.py
+++
@@ -1,8 +0,0 @@
-class Associativo(CacheBySets):
-    def __init__(self, linhas, palavras_linhas, alg_substituicao = 1, politica = 1):
-        super().__init__(linhas, palavras_linhas, alg_substituicao, politica)
-        self.tag = 0 #qual a tag atual
-
-    def calcula(self, adress):
-        ###########
-        self.tag = ####
