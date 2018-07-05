9228e4709ca1093160b79ddc972cc8b4552fbbde
Diego Dantas Figueiredo <35031027+Feelss@users.noreply.github.com>
2018-07-05 11:44:14 -0300

    Update Cache.py

--- Cache.py
+++ Cache.py
@@ -17,22 +17,28 @@ class Conjunto:
 
 class Cache:
     def __init__(self, linhas, palavras_linhas, nro_linhas_conjunto):
-        self.cache_hit = 0 #Acerto de cache
-        self.cache_miss = 0 #Erro de cache
-        self.Memoria = 0 #Acesso a memoria
+        self.cache_hit = 0  # Acerto de cache
+        self.cache_miss = 0  # Erro de cache
+        self.Memoria = 0  # Acesso a memoria
+        self.tam_qtag = 0 # Total de bits destinados a tag
+        self.tam_qconj = 99999999 # Total de bits destinados ao "conjunto"
 
         self.qtd_linhas = linhas
         self.tam_block = palavras_linhas
-        
-        if(nro_linhas_conjunto == -1):
+
+        if (nro_linhas_conjunto == -1):
             nro_linhas_conjunto = linhas
-        
+
         self.num_conjuntos = int(linhas / nro_linhas_conjunto)
         self.cache = [Conjunto(nro_linhas_conjunto) for i in range(self.num_conjuntos)]
-        
 
     def __str__(self):
-        hm = self.cache_hit+self.cache_miss
+        hm = self.cache_hit + self.cache_miss
+        s = 'Bits destinados a tag: ' + str(self.tam_qtag) + '\n'
         if not hm:
             hm = 1
-        return 'Cache Hit: %d (%.2f%%)\nCache Miss: %d (%.2f%%)\nQuantidade de acesso ao barramento: %d' %(self.cache_hit, (self.cache_hit/hm)*100, self.cache_miss, (self.cache_miss/hm)*100, self.Memoria)
+        if self.tam_qconj != 99999999:
+            s += 'Bits destinados a identificação da linha: ' + str(self.tam_qconj) + '\n'
+
+        return 'Cache Hit: %d (%.2f%%)\nCache Miss: %d (%.2f%%)\nQuantidade de acesso ao barramento: %d\n' % (
+        self.cache_hit, (self.cache_hit / hm) * 100, self.cache_miss, (self.cache_miss / hm) * 100, self.Memoria) + s
